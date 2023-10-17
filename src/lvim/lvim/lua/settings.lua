e2c4b7a067fd6da7328befafce1ad58076f094f0
Sharp Zhou <sharp.zhou@shopee.com>
2023-10-17 19:50:34 +0800

    feat: update

--- src/lvim/lvim/lua/settings.lua
+++ src/lvim/lvim/lua/settings.lua
@@ -1,11 +1,11 @@
+vim.cmd.colorscheme "catppuccin-macchiato"
+
 local opts_override = {
   relativenumber = true,
   foldmethod = "expr",
   foldexpr = "nvim_treesitter#foldexpr()",
   clipboard = "",
 }
-
-vim.cmd.colorscheme "catppuccin"
 
 for k, v in pairs(opts_override) do
   vim.opt[k] = v
@@ -14,4 +14,3 @@ end
 vim.cmd [[
   autocmd BufReadPost * lua require'functions'.jump_to_last_pos()
 ]]
-
