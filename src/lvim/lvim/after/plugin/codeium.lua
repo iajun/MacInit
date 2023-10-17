1f125096e617896be429023439c180797ccbbb44
Sharp Zhou <sharp.zhou@shopee.com>
2023-10-17 20:07:10 +0800

    feat: modify codeium mappings

--- src/lvim/lvim/after/plugin/codeium.lua
+++ src/lvim/lvim/after/plugin/codeium.lua
@@ -1,5 +1,10 @@
--- Change '<C-g>' here to any keycode you like.
-vim.keymap.set('i', '<C-y>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-vim.keymap.set('i', '<C-h>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-vim.keymap.set('i', '<C-l>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+local key_mappings = {
+  ["<C-y>"] = function() return vim.fn['codeium#Accept']() end,
+  ["<C-h>"] = function() return vim.fn['codeium#CycleCompletions'](1) end,
+  ["<C-l>"] = function() return vim.fn['codeium#CycleCompletions'](-1) end,
+  ["<C-x>"] = function() return vim.fn['codeium#Clear']() end,
+}
+
+for k, v in pairs(key_mappings) do
+  vim.keymap.set("i", k, v, { expr = true })
+end
