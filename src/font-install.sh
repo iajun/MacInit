a605e9d3607150ddd22c4117e6eba0411730e79b
Sharp Zhou <sharp.zhou@shopee.com>
2024-06-04 09:56:59 +0800

    feat: update font install

--- src/font-install.sh
+++ src/font-install.sh
@@ -1,3 +1,9 @@
-git clone --depth=1 https://github.com/ryanoasis/nerd-fonts
-cd nerd-fonts
+#!/bin/bash
+
+TEMP_DIR=$(mktemp -d)
+
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts $TEMP_DIR
+cd $TEMP_DIR
 bash ./install.sh Meslo
+
+rm -rf $TEMP_DIR
