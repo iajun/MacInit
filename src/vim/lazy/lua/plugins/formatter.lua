6958b91aca67120b4358734ed8c03d81cac9767e
Sharp Zhou <sharp.zhou@shopee.com>
2024-07-23 17:45:32 +0800

    feat: update lazyvim

---
+++ src/vim/lazy/lua/plugins/formatter.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-lspconfig",
+    opts = {
+      setup = {
+        volar = function()
+          require("lazyvim.util").lsp.on_attach(function(client, _)
+            if client.name == "volar" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+}
