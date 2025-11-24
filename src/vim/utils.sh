a9aa003401f63d1b71e500ac99be52f87f9f9362
Sharp Zhou <iveoname@gmail.com>
2025-11-24 22:22:58 +0800

    feat: update

--- src/vim/utils.sh
+++ src/vim/utils.sh
@@ -7,7 +7,7 @@ source "$DIR/../util.sh"
 VERSION="nvim-macos-x86_64"
 
 uninstall_nvim() {
-  rm -rf /usr/local/share/nvim /usr/local/bin/nvim ~/.local/share/nvim ~/.config/nvim ~/.cache/nvim
+  rm -rf /usr/local/share/nvim /usr/local/bin/nvim /usr/local/bin/v ~/.local/share/nvim ~/.config/nvim ~/.cache/nvim
 }
 
 install_nvim() {
@@ -15,7 +15,7 @@ install_nvim() {
   curl -LO https://github.com/neovim/neovim/releases/download/stable/$VERSION.tar.gz
   tar xzf $VERSION.tar.gz -C /usr/local/share
   mv /usr/local/share/$VERSION /usr/local/share/nvim
-  ln -s /usr/local/share/nvim/bin/nvim /usr/local/bin/v
+  ln -sf /usr/local/share/nvim/bin/nvim /usr/local/bin/v
   rm -rf $VERSION.tar.gz
 }
 
@@ -26,7 +26,7 @@ reinstall_nvim() {
 }
 
 uninstall_lvim() {
-  rm -rf ~/.local/share/lvim ~/.local/share/lunarvim* ~/.config/lvim
+  rm -rf ~/.local/share/lvim ~/.local/share/lunarvim* ~/.config/lvim /usr/local/bin/v
 }
 
 install_lvim() {
@@ -35,10 +35,10 @@ install_lvim() {
   }
   command_exists "cargo" install_cargo noop "Cargo is installed, reinstall it?"
   uninstall_lvim
-  ln -s $DIR/lvim ~/.config/lvim
+  ln -sf $DIR/lvim ~/.config/lvim
   LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
-  ln -s ~/.local/bin/lvim /usr/local/bin/v
+  ln -sf $(which nvim) /usr/local/bin/v
 }
 
 install_lazynvim() {
